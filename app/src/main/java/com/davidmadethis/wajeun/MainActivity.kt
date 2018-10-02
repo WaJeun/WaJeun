7d4533842bf0aeccfbbd398ca2d1b01b8703a343
Obi Uchenna David <daviduchenna@outlook.com>
2018-10-02 10:23:25 +0100

    initial commit :fire:

---
+++ app/src/main/java/com/davidmadethis/wajeun/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.davidmadethis.wajeun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davidmadethis.wajeun.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
